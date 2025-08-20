f32070210b4f7382a3f3ba8735aa197eec053f7c
Dharshini <Dharshini@DESKTOP-QF56UAT>
2025-08-20 21:31:23 +0530

    interface example

---
+++ c2tc_javaprogram/src/interface_example/Student.java
@@ -0,0 +1,12 @@
+package interface_example;
+
+
+public class Student implements Coder {
+	
+	@Override
+	public void codingWorks() {
+		System.out.println("Must wakeup at "+wakeuptime);
+		System.out.println("Working with the Eclipse and Github");
+	}
+
+}
